08d7026635e4eff3ca027c296422888b0a6b4e07
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-04-17 20:28:29 -0400

    jdl configured

--- src/main/java/whereismycomic/config/CacheConfiguration.java
+++ src/main/java/whereismycomic/config/CacheConfiguration.java
@@ -48,6 +48,12 @@ public class CacheConfiguration {
             createCache(cm, whereismycomic.domain.User.class.getName());
             createCache(cm, whereismycomic.domain.Authority.class.getName());
             createCache(cm, whereismycomic.domain.User.class.getName() + ".authorities");
+            createCache(cm, whereismycomic.domain.Characters.class.getName());
+            createCache(cm, whereismycomic.domain.Characters.class.getName() + ".series");
+            createCache(cm, whereismycomic.domain.Characters.class.getName() + ".comics");
+            createCache(cm, whereismycomic.domain.Series.class.getName());
+            createCache(cm, whereismycomic.domain.Series.class.getName() + ".comics");
+            createCache(cm, whereismycomic.domain.Comic.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,21 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'characters',
+        data: { pageTitle: 'Characters' },
+        loadChildren: () => import('./characters/characters.module').then(m => m.CharactersModule),
+      },
+      {
+        path: 'series',
+        data: { pageTitle: 'Series' },
+        loadChildren: () => import('./series/series.module').then(m => m.SeriesModule),
+      },
+      {
+        path: 'comic',
+        data: { pageTitle: 'Comics' },
+        loadChildren: () => import('./comic/comic.module').then(m => m.ComicModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
